25aa28deaeacc3883deb89b1f569b39a40279c52
Nicolas Loira <nloira@gmail.com>
2023-01-12 09:44:34 -0300

    Starting with location

---
+++ tubul/tubul_logger.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Nicolas Loira on 1/11/23.
+//
+
+#include "tubul.h"
+#include <stdexcept>
+#include <experimental/source_location>
+
+namespace TU
+{
+std::runtime_error logError(const std::string &msg, const std::source_location location =
+														std::source_location::current())
+{
+
+}
+} // namespace TU
