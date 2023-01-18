94f9790dd6e32f0a904ad96ef75d3b302b5021fc
Carlos Acosta <bigbadrat@gmail.com>
2023-01-18 11:53:57 -0300

    Adding support to split string_views. Had to templatize the split_merge/strict functions but constrained them to work only on strings or string_views.

--- apps/example1/example1.cpp
+++ apps/example1/example1.cpp
@@ -11,5 +11,8 @@ int main(){
 	std::string hello("Hello world 1 2 3");
 	auto tokens = TU::split(hello ) ;
 	std::cout << "I can split and join strings: " << hello << " -> '" << TU::join(tokens,"->") << "'" << std::endl;
+	std::string_view hello_view  = hello;
+	auto tokens_from_view = TU::split(hello_view);
+	std::cout << "Also works with string_views: " << hello_view << " -> '" << TU::join(tokens_from_view,"->") << "'" << std::endl;
 
 }
