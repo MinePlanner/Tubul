73e4205e68100d43f5b5ad4b0554aa67af090828
Nicolas Loira <nloira@gmail.com>
2023-01-26 12:01:39 -0300

    Adds test for logger

--- apps/example1/example1.cpp
+++ apps/example1/example1.cpp
@@ -19,12 +19,12 @@ int main(){
 	auto tokens_from_view = TU::split(hello_view);
 	std::cout << "Also works with string_views: " << hello_view << " -> '" << TU::join(tokens_from_view,"->") << "'" << std::endl;
 
-	std::cout << "With Tubul i can easily iterate simple ranges\n";
+	std::cout << "With Tubul I can easily iterate simple ranges\n";
 	std::vector<std::string> numbers;
 	for (auto i: TU::irange(1,7))
 		numbers.push_back(std::to_string(i));
 	std::cout << TU::join(numbers, "->");
 
-
+	// uncomment to test error location funcionality
 	// error_function();
 }
