a7da7692e58b0fb55e9e36f4b23fd611d34119eb
Carlos Acosta <bigbadrat@gmail.com>
2023-04-12 14:07:36 -0400

    Moving functions to the cpp file and renaming Small change for the running_ flag.

--- tubul/tubul_thread_pool.cpp
+++ tubul/tubul_thread_pool.cpp
@@ -7,22 +7,20 @@ namespace TU
 
 
     ThreadPool::ThreadPool(size_t thread_count) :
-            running_(false),
             tasks_total_(0),
             thread_count_((thread_count > 0) ? thread_count : std::thread::hardware_concurrency()),
-            threads_(std::make_unique<std::thread[]>(thread_count_)),
-            waiting_(false)
+            threads_(std::make_unique<std::thread[]>(thread_count_))
     {
+        running_.test_and_set();
         for (size_t i = 0; i < thread_count_; ++i)
         {
             threads_[i] = std::thread(&ThreadPool::workerFn, this);
         }
-        running_ = true;
     }
 
     ThreadPool::~ThreadPool() {
         waitForTasks();
-        running_ = false;
+        running_.clear();
         task_available_cv_.notify_all();
         for (size_t i = 0; i < thread_count_; ++i)
         {
@@ -31,14 +29,34 @@ namespace TU
     }
 
     void ThreadPool::waitForTasks() {
-        waiting_ = true;
+        waiting_.test_and_set();
         std::unique_lock<std::mutex> tasks_lock(tasks_mutex_);
         task_done_cv_.wait(tasks_lock, [this] { return (tasks_total_ == 0); });
-        waiting_ = false;
+        waiting_.clear();
     }
 
     size_t ThreadPool::threadCount() const {
         return thread_count_;
+    }
+
+    void ThreadPool::workerFn() {
+        while (running_.test())
+        {
+            std::function<void()> task;
+            std::unique_lock<std::mutex> tasks_lock(tasks_mutex_);
+            task_available_cv_.wait(tasks_lock, [this] { return !tasks_.empty() || !running_.test(); });
+            if (running_.test())
+            {
+                task = std::move(tasks_.front());
+                tasks_.pop();
+                tasks_lock.unlock();
+                task();
+                tasks_lock.lock();
+                --tasks_total_;
+                if (waiting_.test())
+                    task_done_cv_.notify_one();
+            }
+        }
     }
 
 }
