5eb49b6b39f915952e2a86dbaa0c01cb43cbdfce
Carlos Acosta <bigbadrat@gmail.com>
2023-08-11 09:21:48 -0400

    Setting the size of the file correctly in the mapped file object and writing content as binary to avoid platform dependant char conversion (\n vs \r\n)

--- tubul/tubul_file_utils.cpp
+++ tubul/tubul_file_utils.cpp
@@ -94,7 +94,7 @@ int strToInt(const std::string_view& p){
             throw TU::Exception(std::string("Could not open file:") + name);
         };
 
-        size_t len  = GetFileSize(fd, 0);
+        size_  = GetFileSize(fd, 0);
 
         mapping = CreateFileMapping(fd, 0, PAGE_READONLY, 0, 0, 0);
         if(mapping == 0) {
